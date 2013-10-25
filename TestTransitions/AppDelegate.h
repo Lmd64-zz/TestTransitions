f0b0348806ff8948fe53c4ed9d438f661b55e6d9
Liam Dunne <liamodunne@gmail.com>
2013-10-25 14:30:26 +0100

    Initial Commit

---
+++ TestTransitions/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestTransitions
+//
+//  Created by Liam Dunne on 25/10/2013.
+//  Copyright (c) 2013 Liam Dunne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TestTransitions/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TestTransitions
+//
+//  Created by Liam Dunne on 25/10/2013.
+//  Copyright (c) 2013 Liam Dunne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
